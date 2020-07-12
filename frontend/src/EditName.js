c7ba31fe7d78d2d0f6434de93d2039f7b8b85467
GaganUnvi <38974523+GaganUnvi@users.noreply.github.com>
2020-07-12 17:38:21 +0530

    remove console logs

--- frontend/src/EditName.js
+++ frontend/src/EditName.js
@@ -5,14 +5,9 @@ const EditName = ({ todo, index }) => {
   const [edited, setEdited] = useState(todo.name);
   const debounceTimeout = useRef(undefined);
 
-  console.log('component rendered');
-
   const editName = async (name) => {
     if (name !== '' || name === undefined) {
       const payload = { name, index };
-
-      console.log('making request', payload);
-
       try {
         const response = await fetch(`${url}editname`, {
           method: 'POST',
@@ -20,7 +15,6 @@ const EditName = ({ todo, index }) => {
           body: JSON.stringify(payload),
         });
         const resJson = await response.json();
-        console.log('REQUEST SUCCESS!', resJson);
       } catch (error) {
         console.log('error', error);
         return;
@@ -36,10 +30,8 @@ const EditName = ({ todo, index }) => {
         setEdited(name);
 
         if (debounceTimeout.current) {
-          console.log('cleared previous timeout');
           clearTimeout(debounceTimeout.current);
         }
-        console.log('set new timeout');
         debounceTimeout.current = setTimeout(() => {
           editName(name);
         }, 1000);
